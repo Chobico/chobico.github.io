caea315ba09bdd5875b3d887b306b4a84dbaa4f6
chobico <lebah.cpp@gmail.com>
2019-11-21 12:14:51 +0700

    UGD12_9383_C

---
+++ precache-manifest.3ea374b0e00604ce1741c99e12cd0bb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1076c3864841f4c52a0",
+    "url": "/css/chunk-vendors.fada67cf.css"
+  },
+  {
+    "revision": "5162dce0ae3734c74d40821400311100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a422d55fa666d2553930",
+    "url": "/js/about.a750cb64.js"
+  },
+  {
+    "revision": "7bd8eb4cc5387b3aaf2f",
+    "url": "/js/app.3c51bcd7.js"
+  },
+  {
+    "revision": "a1076c3864841f4c52a0",
+    "url": "/js/chunk-vendors.e7189a4a.js"
+  },
+  {
+    "revision": "7729466e599eb43878ae8ccba03dcd8b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
